70917c7c469dec8d722239f625ada955b8b94ed7
nbadea00 <nbadea61@gmail.com>
2023-01-10 13:01:13 +0100

    standard-template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProvaComponent } from './components/prova/prova.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prova',
+    component: ProvaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

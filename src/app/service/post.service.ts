9d22decff1662fb75276adac61648c9eca5b411e
nbadea00 <nbadea61@gmail.com>
2023-01-11 14:32:19 +0100

    post-fetch

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -7,43 +7,55 @@ import { Post } from '../interface/post';
 export class PostService {
   constructor() {}
 
+  /*
   posts: Post[] = [
     {
         "id":1,
         "body":"Sit minim deserunt enim enim nisi. Eiusmod incididunt dolore et ea anim. Non exercitation id voluptate et velit quis anim officia fugiat esse laborum nisi fugiat non.",
         "title":"Dolor velit sint tempor culpa cupidatat ipsum do ad tempor eiusmod.",
         "active":true
-    },
-    {
+      },
+      {
         "id":2,
         "body":"Sit minim deserunt enim enim nisi. Eiusmod incididunt dolore et ea anim. Non exercitation id voluptate et velit quis anim officia fugiat esse laborum nisi fugiat non.",
         "title":"Dolor velit sint tempor culpa cupidatat ipsum do ad tempor eiusmod.",
         "active":true
-    },
-    {
+      },
+      {
         "id":3,
         "body":"Sit minim deserunt enim enim nisi. Eiusmod incididunt dolore et ea anim. Non exercitation id voluptate et velit quis anim officia fugiat esse laborum nisi fugiat non.",
         "title":"Dolor velit sint tempor culpa cupidatat ipsum do ad tempor eiusmod.",
         "active":true
-    },
-    {
+      },
+      {
         "id":4,
         "body":"Sit minim deserunt enim enim nisi. Eiusmod incididunt dolore et ea anim. Non exercitation id voluptate et velit quis anim officia fugiat esse laborum nisi fugiat non.",
         "title":"Dolor velit sint tempor culpa cupidatat ipsum do ad tempor eiusmod.",
         "active":false
-    },
-    {
+      },
+      {
         "id":5,
         "body":"Sit minim deserunt enim enim nisi. Eiusmod incididunt dolore et ea anim. Non exercitation id voluptate et velit quis anim officia fugiat esse laborum nisi fugiat non.",
         "title":"Dolor velit sint tempor culpa cupidatat ipsum do ad tempor eiusmod.",
         "active":false
-    }
+      }
   ];
+  */
 
   getFilterPosts(param:boolean): Post[] {
     return this.posts.filter((element:Post) => element.active ==  param)
   }
 
+  b = this.getPost()
+
+  posts:Post[] = [];
+
+  async getPost(){
+    let post:Post[] = []
+    let a = await fetch("assets/db.json").then((response)=> response.json()).then((data) => post = data )
+    this.posts =  post;
+    console.log(this.posts);
+  }
   /*
   async getFilterPosts(param:boolean): Promise<Post[]> {
     return await fetch("assets/db.json").then((response)=> response.json()).then((data) => {return data.filter((element:Post) => element.active === param)});
